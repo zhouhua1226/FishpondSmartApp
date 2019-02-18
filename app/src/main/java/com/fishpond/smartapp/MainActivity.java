fc723c500a15e73052ddf57501be2705f2542cdf
zhouhua <zhouhua@songguo7.com>
2019-02-18 15:40:55 +0800

    some commits v1.0.2

--- app/src/main/java/com/fishpond/smartapp/MainActivity.java
+++ app/src/main/java/com/fishpond/smartapp/MainActivity.java
@@ -96,7 +96,7 @@ public class MainActivity extends Activity {
         connectService();
         NettyUtils.registerAppManager();
         initWelcome();
-        Log.e("ty", "====" + CmdType.getSceneName(1, "ty"));
+        Log.e("ty", "====" + CmdType.getSceneName(1, "tys"));
     }
 
     @OnClick({R.id.login_iv})
